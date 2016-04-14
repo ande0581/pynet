fae0359357f21c4b2e7139bfd91b6b9499964a5b
Jeff Anderson <jeffrey.d.anderson@gmail.com>
2016-04-14 05:40:55 -0700

    Fix network dict value in list

--- class1/class1_ex6.py
+++ class1/class1_ex6.py
@@ -2,7 +2,7 @@
 import yaml
 import json
 
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, "Firewall", "Router", "Switch", {"network": "10.11.12.1", "subnet": "255.255.255.0",
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, "Firewall", "Router", "Switch", {"network": "10.11.12.0", "subnet": "255.255.255.0",
                                                                     "gateway": "10.11.12.1"}, {"Model": "WS3560",
                                                                                                "Vendor": "Cisco"}]
 

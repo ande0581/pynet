b8a0fa6b7b0a7f16f369dba0d42c19a4cda09c72
Jeff Anderson <jeffrey.d.anderson@gmail.com>
2016-04-19 19:48:40 -0700

    initial commit

---
+++ class2/class2_ex2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import telnetlib
+import time
+
+IP_ADDRESS = '50.76.53.27'
+TELNET_PORT = 23
+TELNET_TIMEOUT = 6
+
+
+def main():
+    remote_conn = telnetlib.Telnet(IP_ADDRESS, TELNET_PORT, TELNET_TIMEOUT)
+    
+    remote_conn.read_until('sername', TELNET_TIMEOUT)
+    remote_conn.write('pyclass' + '\n')
+    remote_conn.read_until('assword', TELNET_TIMEOUT)
+    remote_conn.write('88newclass' + '\n')
+
+    remote_conn.write('show ip int brief' + '\n')
+    time.sleep(1)
+    output = remote_conn.read_very_eager() 
+    print output
+
+main()
